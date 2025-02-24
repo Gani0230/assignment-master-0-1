16008b04190480e2addd1026c0296d3de6a214ef
Gani0230 <ganeshjadhav7478@gmail.com>
2025-02-24 23:18:35 +0530

    partially solved mongo problem only 2 routes left

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,18 +1,42 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
+const { default: mongoose } = require("mongoose");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup',async (req, res) => {
     // Implement user signup logic
+    let username = req.body.username;
+    let password = req.body.password;
+    await User.create({
+        username: username,
+        password: password
+    })
+
+    res.status(200).json({
+        msg:"User created successfully"
+    })
 });
 
-router.get('/courses', (req, res) => {
+
+router.get('/courses',async (req, res) => {
     // Implement listing all courses logic
+    let data = await User.find({})
+    res.status(200).json({
+        data
+    })
 });
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+    let id = req.params.courseId;
+
+    let course = Course.findOne({
+        _id: id
+    })
+    res.status(200).json({
+        course
+    })
 });
 
 router.get('/purchasedCourses', userMiddleware, (req, res) => {
